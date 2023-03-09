ac194d9b99313ce95204373b336640d7def1896c
Your NameTejas <tejabhai1395@gmail.com>
2023-03-09 22:28:35 +0530

    Added New API

--- new_project/main.py
+++ new_project/main.py
@@ -16,6 +16,9 @@ def predict_price():
     
     return str(predicted_price)
 
+@app.route('/tejas')
+def my_name():
+    return "Done by Tejas"
 
 if __name__ == "__main__":
     app.run(debug=True, port = 5050, host='127.0.0.100')
